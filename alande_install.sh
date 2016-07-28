35fccc3a5254cc862bfded2bc827c9374bff1fcb
areed <alan_p_reed@hotmail.co.uk>
2016-07-28 22:33:14 +0100

    Fixed config of libfm, synapse and GTK

--- alande_install.sh
+++ alande_install.sh
@@ -5,14 +5,13 @@
 # any required services for the DE.
 
 # #AUR Dependencies:
- aur_list=(	'numix-icon-theme-git' 'numix-circle-icon-theme-git' 'midori-flash'	)	# Icon theme
+ aur_list=(	'numix-icon-theme-git' 'numix-circle-icon-theme-git' 'nspluginwrapper' 'midori-flash'	)	# Icon theme
 
 repo_list=(	'metacity'		# WM and compositor
 			'numix-themes' 			# GTK theme
 			'ttf-dejavu' 'noto-font'	# Font
 			'synapse'				# Application launcher
 			'lxappearance' 	# GTK theming GUI
-			# 'lxappearance-obconf' 'openbox' 'compton'	#Trying out metacity instead of openbox
 			'lxpanel' 'lxinput' 'lxrandr' 'lxtask'
 			'lxsession'		# Session manager
 			'terminator'	# Terminal
@@ -24,13 +23,8 @@ repo_list=(	'metacity'		# WM and compositor
 
 aur_address='https://aur.archlinux.org/'
 
-# #Check that we have root access
-# if [ "$(whoami)" != "root" ]; then
-# 	echo "Please run this as root!"
-# 	exit 1
-# fi
 
-#Check that pacman is available
+# Check that pacman is available
 if  !(hash pacman 2>/dev/null);  then
 	echo "Pacman isn't installed, are you sure you're using Arch Linux?"
 	exit 1
@@ -39,14 +33,14 @@ fi
 echo "Updating repositories prior to installation."
 sudo pacman -Syu --noconfirm
 
-#Make sure git is available for handling AUR packages
+# Make sure git is available for handling AUR packages
 if  !(hash git 2>/dev/null);  then
 	echo "Git not installed. Fixing:"
 	sudo pacman -S git --noconfirm
 fi
 
 
-# #Install AUR packages
+# Install AUR packages
 for pkg in "${aur_list[@]}"; do
 	if !(pacman -Q $pkg 1>/dev/null); then
 		echo "Installing $pkg "
@@ -59,7 +53,7 @@ for pkg in "${aur_list[@]}"; do
 	fi
 done
 
-#Install repository packages
+# Install repository packages
 for pkg in "${repo_list[@]}"; do
 	if !(pacman -Q $pkg 1>/dev/null); then
 		echo "Installing $pkg"
@@ -79,5 +73,13 @@ sudo systemctl enable lightdm.service
 # directory to work, plus I can't get the PKGBUILD or .install script to create these in a nicer way.
 
 # GTK tweaks to fix Metacity title bar size
-ln -s "/etc/gtk-3.0/gtk.css" "$HOME/.config/gtk-3.0/gtk.css"
+mkdir -p $HOME/.config/gtk-3.0/
+ln -fs "/etc/gtk-3.0/gtk.css" "$HOME/.config/gtk-3.0/gtk.css"
 
+# Custom Synapse config file
+mkdir -p $HOME/.config/synapse/
+ln -fs "/etc/xdg/synapse/config.json" "$HOME/.config/synapse/config.json"
+
+# libfm config - this affects PCManFM
+mkdir -p $HOME/.config/libfm/
+ln -fs "/etc/xdg/libfm/libfm.conf" "$HOME/.config/libfm/libfm.conf"
